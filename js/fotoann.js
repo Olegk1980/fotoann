9468dc1d918812c56796c51c54b250112a7c24ba
Oleg Knyazev <Oleg Knyazev>
2017-12-21 00:25:41 +0700

    init1

--- js/fotoann.js
+++ js/fotoann.js
@@ -1,8 +1,29 @@
 'use strict';
 
-function RequestAlbum() {
+function CreateParamLink(numPage) {
+  let param;
+  switch (numPage) {
+    case 1:
+      param = 'aid=863420403159&application_key=CBAEGMAMEBABABABA&count=100&fields=photo.PIC180MIN&format=json&method=photos.getPhotos&sig=722118adf757acc4b2d411a0da052d1a&access_token=tkn14fszGBpmxBiKNC32otYB2U02sW5j5o6LxlMlBMAdMdhuWBp1HEFm3sQ38g2ky5az3';
+      break;
+    case 4:
+      alert('В точку!');
+      break;
+    case 5:
+      alert('Перебор');
+      break;
+    default:
+      alert('Я таких значений не знаю');
+  }
+  if (param != undefined) {
+    RequestAlbum(param)
+  }
 
-  httpGet('https://api.ok.ru/fb.do?aid=858510026711&application_key=CBAEGMAMEBABABABA&count=100&format=json&method=photos.getPhotos&sig=7e3cd610ed36beaf2b45e832f684dfa4&access_token=tkn14fszGBpmxBiKNC32otYB2U02sW5j5o6LxlMlBMAdMdhuWBp1HEFm3sQ38g2ky5az3')
+}
+
+function RequestAlbum(param) {
+
+  httpGet('https://api.ok.ru/fb.do?' + param)
 
   .then(JSON.parse,
 
@@ -32,11 +53,11 @@ function RequestAlbum() {
 
     for (var i in okAlbum.photos) {
 
-      //  console.log(i, okAlbum.photos[i].pic640x480);
+      //  console.log(i, okAlbum.photos[i].pic180min);
 
       let img = new Image();
 
-      img.src = okAlbum.photos[i].pic640x480;
+      img.src = okAlbum.photos[i].pic180min;
 
       let link = document.createElement('a');
 
